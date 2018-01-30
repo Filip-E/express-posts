ffd1ded3f4553c10153ba323a2dd71e7060428b7
FilipElen <filipelengit@outlook.com>
2018-01-30 15:53:30 +0100

    search done. everything works

--- server.js
+++ server.js
@@ -21,19 +21,31 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
 	request('http://jsonplaceholder.typicode.com/posts', function(error, response, body){
 		 if (error) return console.log(error);
-		 var test = JSON.parse(body);
-		 res.render('posts.ejs', {posts: test});
+		 var json = JSON.parse(body);
+		 res.render('posts.ejs', {posts: json});
 	});
 });
 
 // Show the search form
 app.get('/search', (req, res) => {
-   res.render('search.ejs', { post: '' });
+	res.render('search.ejs', { post: '' });
 });
 
 // Find all comments for post
 app.post('/search', (req, res) => {
-	 var query = { name: req.body }
-	 console.log(query);
-	
+	 var query = { title: req.body.title }
+		request('http://jsonplaceholder.typicode.com/posts', function(error, response, body){
+			 if (error) return console.log(error);
+			 var json = JSON.parse(body);
+			 for(i = 0; i < json.length; i++){
+				 if(json[i].title == query.title){
+					 var url = 'http://jsonplaceholder.typicode.com/post/' + json[i].id + '/comments';
+					 request(url, function(err, resp, bod){
+						 var commentsJSON = JSON.parse(bod);
+						 res.render('search_result.ejs', {comments: commentsJSON}); 
+					 });
+				 } 
+				 
+			 }
+		});
 });
